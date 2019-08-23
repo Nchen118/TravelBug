f509a90aea41e62d8e9b609be421ec2695d18579
Nchen118 <Nchen118@users.noreply.github.com>
2019-08-24 01:07:09 +0800

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import travelBug.UI.*;
+import travelBug.library.library;
+
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+public class Main {
+	public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+    		try {
+    			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+    		} catch (Exception e) { 
+    			library.dialogMessage(e.getMessage());
+    		}
+    		new Authentication();
+        });
+	}
+
+}
+ 
